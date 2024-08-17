c03ca7a4abaee820fecf6cde5a19cc3d1dbc72e1
Kamaal Farah <kamaal.f1@gmail.com>
2024-08-17 14:52:29 +0200

    Initial commit

---
+++ Swift6ConcurrencyResolver/Swift6ConcurrencyResolverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift6ConcurrencyResolverApp.swift
+//  Swift6ConcurrencyResolver
+//
+//  Created by Kamaal M Farah on 17/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swift6ConcurrencyResolverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
